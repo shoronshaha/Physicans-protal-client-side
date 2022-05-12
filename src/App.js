9b7080e25062b3ea90613a9152c11d15b43904e3
shoronshaha <smaranshaha@gmail.com>
2022-05-12 23:58:17 +0600

    added calender and pic present time and what you want you can select it

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home/Home/Home';
 import About from './pages/About/About';
 import Login from './pages/Login/Login';
+import Appointment from './pages/Appointment/Appointment';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="home" element={<Home />} />
+        <Route path="appointment" element={<Appointment />} />
         <Route path="about" element={<About />} />
         <Route path="login" element={<Login />} />
       </Routes>
