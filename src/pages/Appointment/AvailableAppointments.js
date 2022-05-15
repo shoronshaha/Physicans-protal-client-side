b887c4e971b7083bcaae01afa713c96b2793cbd2
shoronshaha <smaranshaha@gmail.com>
2022-05-15 19:56:18 +0600

    added the url for the available booking slots

--- src/pages/Appointment/AvailableAppointments.js
+++ src/pages/Appointment/AvailableAppointments.js
@@ -7,8 +7,10 @@ const AvailableAppointments = ({ date, setDate }) => {
     const [services, setService] = useState([]);
     const [treatment, setTreatment] = useState(null);
 
+    const formattedDate = format(date, 'PP');
+
     useEffect(() => {
-        fetch('http://localhost:5000/service')
+        fetch(`http://localhost:5000/available?date=${formattedDate}`)
             .then(res => res.json())
             .then(data => setService(data));
     })
